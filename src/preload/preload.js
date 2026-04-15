2561af9b8eaf0a841a45f352e84363d57eb73619
Duong-Thai1 <duongquocthai981@gmail.com>
2026-04-15 11:32:17 +0700

    Build: Set up Electron project with testing

---
+++ src/preload/preload.js
@@ -0,0 +1,9 @@
+const { contextBridge } = require('electron');
+/*const {compute, formatNumber, symbolForOp} = require('../shared/calculator');
+
+contextBridge.exposeInMainWorld('calc', {
+    compute,
+    formatNumber,
+    symbolForOp
+})
+*/
